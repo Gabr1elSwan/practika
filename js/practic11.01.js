2373ef5017f29a2c1288d9297ac555741f1cb147
Gabr1elSwan <73124668+Gabr1elSwan@users.noreply.github.com>
2021-01-11 16:43:45 +0200

    1

--- js/practic11.01.js
+++ js/practic11.01.js
@@ -1,8 +1,37 @@
-let arr = [1, "от", 2, 3, 4, 5, 6, 7, 8, 9, "до", 10];
-const delate = function (arr, from, to) {
-    let first = arr.indexOf(from);
-    let second = arr.indexOf(to);
-    const newArr = arr.splice(first, (second + 1 - first));
-    return newArr;
-};
-delate(arr, 'от', 'до');
+// let a = [1, 3, 5, 6, 9];
+// let b = [];
+
+// const isEven = function (n) {
+//   return n % 2 === 0;
+// };
+
+// const fn = function (arr) {
+//   for (const num of arr) {
+//     isEven(num) ? b.push(num) : '';
+//   }
+//   return b;
+// };
+
+// console.log(fn(a));
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// const pow = function (n, q) {
+//   let result = 1;
+//   for (let i = 0; i < q; i++) {
+//     result *= n;
+//   }
+//   console.log(result);
+// };
+// pow(3, 3);
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// const pow = function (n, q) {
+//   if (q === 1) {
+//     return n;
+//   } else {
+//     return n * pow(n, (q -= 1));
+//   }
+// };
+// console.log(pow(4, 3));
