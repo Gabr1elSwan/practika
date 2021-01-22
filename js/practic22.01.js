5010c74c7c7ca65627cf13cab81d4f4e54e8e705
Gabr1elSwan <73124668+Gabr1elSwan@users.noreply.github.com>
2021-01-22 15:56:37 +0200

    !

--- js/practic22.01.js
+++ js/practic22.01.js
@@ -1,23 +1,22 @@
 import gallery from './data.js';
-
 const divRef = document.querySelector('.container');
-
-const imgRef = document.querySelector('.img');
-
-const opton = {
+const imgRef = document.querySelectorAll('.img');
+const options = {
   root: divRef,
-  rootMargin: '',
-  threshold: 0.5,
+  rootMargin: '50px',
+  threshold: 0.1,
 };
-
-const changImg = (entries, gallery) => {
+const changImg = entries => {
+  imgRef.forEach((elem, i) => {
+    elem.dataset.url = gallery[i].src;
+  });
   entries.forEach(element => {
-    console.log(element);
+    element.isIntersecting
+      ? (element.target.src = element.target.dataset.url)
+      : '';
   });
 };
-
-const observer = new IntersectionObserver(changImg, opton);
-
+const observer = new IntersectionObserver(changImg, options);
 imgRef.forEach(elem => {
   observer.observe(elem);
 });
