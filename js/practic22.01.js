c59b092fc9dfeefd06aa6b55c1cb883c64afb068
Gabr1elSwan <73124668+Gabr1elSwan@users.noreply.github.com>
2021-01-27 17:37:50 +0200

    1

--- js/practic22.01.js
+++ js/practic22.01.js
@@ -1,22 +1,22 @@
-import gallery from './data.js';
-const divRef = document.querySelector('.container');
-const imgRef = document.querySelectorAll('.img');
-const options = {
-  root: divRef,
-  rootMargin: '50px',
-  threshold: 0.1,
-};
-const changImg = entries => {
-  imgRef.forEach((elem, i) => {
-    elem.dataset.url = gallery[i].src;
-  });
-  entries.forEach(element => {
-    element.isIntersecting
-      ? (element.target.src = element.target.dataset.url)
-      : '';
-  });
-};
-const observer = new IntersectionObserver(changImg, options);
-imgRef.forEach(elem => {
-  observer.observe(elem);
-});
+// import gallery from './data.js';
+// const divRef = document.querySelector('.container');
+// const imgRef = document.querySelectorAll('.img');
+// const options = {
+//   root: divRef,
+//   rootMargin: '50px',
+//   threshold: 0.1,
+// };
+// const changImg = entries => {
+//   imgRef.forEach((elem, i) => {
+//     elem.dataset.url = gallery[i].src;
+//   });
+//   entries.forEach(element => {
+//     element.isIntersecting
+//       ? (element.target.src = element.target.dataset.url)
+//       : '';
+//   });
+// };
+// const observer = new IntersectionObserver(changImg, options);
+// imgRef.forEach(elem => {
+//   observer.observe(elem);
+// });
